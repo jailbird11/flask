cf28f27a6ab96f3f8c7249fc42592e773879f1aa
Sophie <sophiehickey123@gmail.com>
2020-06-16 10:42:59 +0000

    created 3 pages using jinja template

--- flask/run.py
+++ flask/run.py
@@ -1,16 +1,25 @@
 # imports flask
 import os
-from flask import Flask 
+from flask import Flask, render_template 
 
 # Creates an instances of this and stores in an variable called app
 app = Flask(__name__) 
 
-# app route decorator-starts with @
+# app route decorator-starts with @. Links to pages
 @app.route("/") 
-def indx():
-    return "Hello, World"
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True) #Only use debug for testing not for project
+            debug=True) 
+# Only use debug for testing not for project
